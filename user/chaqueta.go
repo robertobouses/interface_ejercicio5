6910cb6ee947d1430acc94dc58cd95e9d4620372
Roberto Fernandez Toro <robertobouses@gmail.com>
2023-05-26 21:05:46 +0200

    no funciona

--- user/chaqueta.go
+++ user/chaqueta.go
@@ -1,22 +1,22 @@
 package user
 
-type Camiseta struct {
+type Chaqueta struct {
 	Nombre   string
 	Unidades int
 	Precio   float32
 	Marca    string
 }
 
-func (C Camiseta) ImporteStock() float32 {
+func (C Chaqueta) ImporteStock() float32 {
 	Stock := float32(C.Unidades) * C.Precio
 	return Stock
 }
 
-func (C Camiseta) ImporteIva(Stock float32) float32 {
+func (C Chaqueta) ImporteIva(Stock float32) float32 {
 	Iva := (Stock) * 0.21
 	return Iva
 }
 
-func (C Camiseta) Datos() (string, string) {
-	return C.Nombre, C.Color
+func (C Chaqueta) Datos() (string, string) {
+	return C.Nombre, C.Marca
 }
--- user/pantalon.go
+++ user/pantalon.go
@@ -1,9 +1,24 @@
 package user
 
+import "fmt"
+
 type Pantalon struct {
 	Nombre   string
 	Unidades int
-	Precio   int
-	Iva      int
-	Importe  int
+	Precio   float32
+	Marca    string
+}
+
+func (P Pantalon) ImporteStock() int {
+	Stock := P.Unidades * int(P.Precio)
+	return Stock
+
+}
+func (P Pantalon) ImporteIva(float32) float32 {
+	fmt.Println("Los pantalones viene de importación sin IVA")
+	return 0
+}
+
+func (P Pantalon) Datos() string {
+	return P.Nombre
 }
